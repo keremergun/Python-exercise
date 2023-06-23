8fe2bc912e3e9761f014cf4bbf609c35cff4206f
Kerem Ergun <kerem172009@gmail.com>
2023-06-23 14:57:13 +0300

    Defined Health Status function

--- Quiz1/Quiz1.py
+++ Quiz1/Quiz1.py
@@ -7,3 +7,16 @@ single_point = int(sys.argv[3])
 total_point = two_point*2 + three_point*3 + single_point
 
 print(total_point)
+
+def healthStatus(heigth, mass):
+    bmi = float(mass) / float(heigth)**2
+    
+    if bmi < 18.5:
+        return "Underweight"
+    elif bmi >= 18.5 and bmi < 24.9:
+        return "Healthy"
+    elif bmi >=24.9 and bmi <30:
+        return "Overweight"
+    else:
+        return "Obese"
+    
