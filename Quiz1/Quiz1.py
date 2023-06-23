e721ac094e8807529683c6418d615171f274780a
Kerem Ergun <kerem172009@gmail.com>
2023-06-23 14:43:52 +0300

    Problem 1 done!

--- Quiz1/Quiz1.py
+++ Quiz1/Quiz1.py
@@ -1,6 +1,9 @@
 import sys
 
-two_point = sys.argv[1]
-three_point = sys.argv[2]
-single_point = sys.argv[3]
+two_point = int(sys.argv[1])
+three_point = int(sys.argv[2])
+single_point = int(sys.argv[3])
 
+total_point = two_point*2 + three_point*3 + single_point
+
+print(total_point)
