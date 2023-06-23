e7de0b25b39723e353b03c8250d3f792b58ee9d3
Kerem Ergun <kerem172009@gmail.com>
2023-06-23 15:03:15 +0300

    Problem 2 done!

--- Quiz1/Quiz1.py
+++ Quiz1/Quiz1.py
@@ -1,12 +1,15 @@
 import sys
+try:
+    two_point = int(sys.argv[1])
+    three_point = int(sys.argv[2])
+    single_point = int(sys.argv[3])
 
-two_point = int(sys.argv[1])
-three_point = int(sys.argv[2])
-single_point = int(sys.argv[3])
+    total_point = two_point*2 + three_point*3 + single_point
 
-total_point = two_point*2 + three_point*3 + single_point
+    print(total_point)
 
-print(total_point)
+except IndexError:
+    print()
 
 def healthStatus(heigth, mass):
     bmi = float(mass) / float(heigth)**2
